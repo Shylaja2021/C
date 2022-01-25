7c3b8e48dbd1b33e5c30de64ccafb9f03ae8e320
Shylaja2021 <98077969+Shylaja2021@users.noreply.github.com>
2022-01-25 13:30:47 +0530

    triangle pattern program

---
+++ triangle.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void main(){
+int num,i,j,k=1;
+printf("Enter the value of num : ");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        printf("*");
+    }
+    printf("\n");
+}
+printf("=============\n");
+for(i=1;i<=num;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        printf("%d",j);
+    }
+    printf("\n");
+}
+printf("=============\n");
+for(i=1;i<=num;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        printf("%d",i);
+    }
+    printf("\n");
+}
+printf("=============\n");
+for(i=1;i<=num;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        printf("%d",k++);
+    }
+    printf("\n");
+}
+}
