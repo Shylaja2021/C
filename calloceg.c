7ce076f24efbe2bd366e1864365af819fa176004
Shylaja2021 <98077969+Shylaja2021@users.noreply.github.com>
2022-02-01 20:35:55 +0530

    allocate memory to array(calloc)

---
+++ calloceg.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+   int i, n;
+   int *a;
+   printf("Number of elements to be entered:");
+   scanf("%d",&n);
+
+   a = (int*)calloc(n, sizeof(int));
+   printf("start memory %lu\n",a);
+   printf("end memory %lu\n",a+n);
+   printf("Enter %d numbers:\n",n);
+   for( i=0 ; i < n ; i++ ) {
+      scanf("%d",&a[i]);
+   }
+
+   printf("The numbers entered are: ");
+   for( i=0 ; i < n ; i++ ) {
+      printf("%d ",a[i]);
+   }
+   free( a );
+   printf("\nNumber of elements to be entered:");
+   scanf("%d",&n);
+   a = (int*)calloc(n, sizeof(int));
+   printf("\nstart memory %lu\n",a);
+   printf("end memory %lu\n",a+n);
+   printf("Enter %d numbers:\n",n);
+   for( i=0 ; i < n ; i++ ) {
+      scanf("%d",&a[i]);
+   }
+
+   printf("The numbers entered are: ");
+   for( i=0 ; i < n ; i++ ) {
+      printf("%d ",a[i]);
+   }
+   free( a );
+
+   return(0);
+}
